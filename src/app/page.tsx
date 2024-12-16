7ad99b25174118f668d86e0e1b207b25b8209f1b
creativesar <uniqueluck68@gmail.com>
2024-12-16 23:29:57 +0000

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -226,7 +226,7 @@ export default function Home() {
         <ProductListSec
           title="NEW ARRIVALS"
           data={newArrivalsData}
-          viewAllLink="/shop#new-arrivals"
+          viewAllLink="/#new-arrivals"
         />
         <div className="max-w-frame mx-auto px-4 xl:px-0">
           <hr className="h-[1px] border-t-black/10 my-10 sm:my-16" />
@@ -235,7 +235,7 @@ export default function Home() {
           <ProductListSec
             title="top selling"
             data={topSellingData}
-            viewAllLink="/shop#top-selling"
+            viewAllLink="/#top-selling"
           />
         </div>
         <div className="mb-[50px] sm:mb-20">
